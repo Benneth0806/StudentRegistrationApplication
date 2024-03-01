72d293bd1a34fcfaa4063d9ce9e1d479c28b07e1
Benneth0806 <151595874+Benneth0806@users.noreply.github.com>
2024-03-01 16:45:07 +0800

    Update Form1.cs

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,4 +1,4 @@
-using Microsoft.VisualBasic.FileIO;
+ using Microsoft.VisualBasic.FileIO;  
 
 namespace WinFormsApp1
 {
@@ -15,13 +15,15 @@ namespace WinFormsApp1
             string Firstname = textBox2.Text;
             string Middlename = textBox3.Text;
 
-            string day = comboBox1.GetItemText(comboBox1.SelectedIndex);
-            string month = comboBox2.GetItemText(comboBox2.SelectedIndex);
-            string year = comboBox3.GetItemText(comboBox3.SelectedIndex);
+            string day = comboBox1.GetItemText(comboBox1.SelectedItem);
+            string month = comboBox2.GetItemText(comboBox2.SelectedItem);
+            string year = comboBox3.GetItemText(comboBox3.SelectedItem);
+
+            string Program = comboBox4.GetItemText(comboBox4.SelectedItem);
 
             string Gender = radioButton1.Checked == true ? "Male" : "Female";
 
-            MessageBox.Show("Studet: " + Firstname + " " + Middlename + " " + Lastname + "\nGender: " + Gender + "\nDate Of Birth: " + day + "/" + month + "/" + year);
+            MessageBox.Show("Student: " + Firstname + " " + Middlename + " " + Lastname + "\nGender: " + Gender + "\nDate Of Birth: " + day + "/" + month + "/" + year + "\nProgram: " + Program);
 
         }
 
@@ -41,15 +43,23 @@ namespace WinFormsApp1
             {
                 comboBox1.Items.Add(i.ToString());
             }
-            for (int i = 1; i <= 12; i++)
+
+            string[] months = { "January", "February", "March", "April", "June", "July", "August", "September", "Otocber", "November", "December" };
+            foreach (string i in months)
             {
-                comboBox2.Items.Add(i.ToString());
+                comboBox2.Items.Add(i.ToString());  
             }
+
             for (int i = 1900; i <= 2024; i++)
             {
                 comboBox3.Items.Add(i.ToString());
             }
 
+            string[] listOfProgram = { "Bachelor of Science in Computer Science", "Bachelor of Science in Information Technology", "Bachelor of Science in Information Technology", "Bachelor of Science in Computer Engineering" };
+            foreach (string i in listOfProgram)
+            {
+                comboBox4.Items.Add(i.ToString());
+            }
 
         }
 
@@ -81,6 +91,36 @@ namespace WinFormsApp1
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+ 
         }
     }
 }
